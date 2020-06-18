306eaf345a90ed6c3aaa5d3ea95dd0b3410b4e73
Alexey Suponin <lexxsmith@gmail.com>
2020-06-18 12:12:21 +0700

    bootstrap with db create and admin init, closes #53

--- flaskCMS.py
+++ flaskCMS.py
@@ -7,3 +7,15 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Project': Project}
+
+
+@app.cli.command('bootstrap')
+def bootstrap():
+    db.create_all()
+
+    u = User(username='admin', email='admin@example.com', confirmed=True)
+    u.set_password('1q2w3e')
+
+    db.session.add(u)
+    db.session.commit()
+
